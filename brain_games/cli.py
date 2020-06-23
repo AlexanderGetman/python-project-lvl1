cf4fe9a8e733759984e9bc938b753b389ae150fc
Александр Гетман <alexander_getman@mail.ru>
2020-06-23 22:15:40 +1000

    step three

---
+++ brain_games/cli.py
@@ -0,0 +1,7 @@
+import prompt
+import logging
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+    logging.warning("Hello, " + name + "!")
